a15e7cced5ed2f451c8832cae4333dde57462809
Edgar Honing <edgar.honing@ahead.com>
2023-11-15 23:24:22 +0000

    first attempt at data reading; note ACL related resources in template

--- data_ingest/ingest.py
+++ data_ingest/ingest.py
@@ -1,3 +1,4 @@
+import boto3
 import os
 import logging
 
@@ -5,8 +6,26 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
+s3_client = boto3.client('s3')
+
+def read_file(bucket, key):
+    try:
+        response = s3_client.get_object(Bucket=bucket, Key=key)
+        contents = response['Body'].read()
+        return f"Length of read file contents: {len(contents)}"
+    except Exception as e:
+        return f"Failed to read contents of {bucket} / {key} due to: {e}"
+    
+    
 def lambda_handler(event, context):
-    logger.info("=== full event details: ===")
-    logger.info(event)
-    logger.info("=== focussed event details: ===")
-    logger.info(event.Records[0].s3)
+    log_info = ""
+    log_info += "=== full event details: ===\n"
+    log_info += f"{event}\n"
+    logger.info(log_info)
+    log_info = ""
+    for r in event['Records']:
+        bucketname = r['s3']['bucket']['name']
+        newfilename = r['s3']['object']['key']
+        log_info += f"Bucket / new file: {bucketname} / {newfilename}"
+        log_info += f"{read_file(bucketname, newfilename)}\n"
+    logger.info(log_info)
