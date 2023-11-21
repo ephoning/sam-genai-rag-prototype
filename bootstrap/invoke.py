4cef446dcea495dd6afdd6b6d52979a15b365c4d
Edgar Honing <edgar.honing@ahead.com>
2023-11-21 16:12:12 +0000

    fix env var refs

--- bootstrap/invoke.py
+++ bootstrap/invoke.py
@@ -9,20 +9,21 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
+index_name = os.environ['DEFAULT_PINECONE_INDEX_NAME']
+api_key = os.environ["DEFAULT_PINECONE_API_KEY"]
+environment = os.environ["DEFULT_PINECONE_ENVIRONMENT"]
+dimension = int(os.environ["EMBEDDING_DIMENSION"])
+metric = os.environ["DEFAULT_PINECONE_METRIC"]
+
+
 def lambda_handler(event, context):
-    
-    index_name = os.environ['PINECONE_INDEX_NAME']
-    api_key = os.environ["PINECONE_API_KEY"]
-    environment = os.environ["PINECONE_ENVIRONMENT"]
-    dimension = int(os.environ["EMBEDDING_DIMENSION"])
-    
     logger.info(f"Initialize Pincone index with {index_name}/{api_key}/{environment}/{dimension}")
-    create_pinecone_index(index_name, api_key, environment, dimension, metric='dotproduct', verbose=True)
+    create_pinecone_index(index_name, api_key, environment, dimension, metric=metric, verbose=True)
 
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": f"Initialized Pincone index with {index_name}/{api_key}/{environment}/{dimension}",
+            "message": f"Initialized Pincone index with {index_name}/{api_key}/{environment}/{dimension}/{metric}",
             # "location": ip.text.replace("\n", "")
         }),
     }
